826d08b699bdafebac4d7a89ce954cd4a79d57ec
Michal <michal.dysput@gmail.com>
2019-05-07 09:56:21 +0200

    Order added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'cards/show'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :shops, only:[:index, :show]
+  resources :order_items
+  resources :cards, only:[:show]
 
   root'shops#index'
 end
